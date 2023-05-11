615d660698689605e8f55538e8da2f1a9e048b6a
nandobfer <nandobfer@yahoo.com.br>
2023-05-11 15:21:31 -0300

    db: added products model

---
+++ prisma/migrations/20230511182055_products/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `products` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `stock` INTEGER NOT NULL,
+    `story` TEXT NOT NULL,
+    `price` DOUBLE NOT NULL,
+    `image` TEXT NOT NULL,
+    `video` TEXT NOT NULL,
+    `specifications` TEXT NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
