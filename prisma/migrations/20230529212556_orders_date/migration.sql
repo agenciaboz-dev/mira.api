0c127f12fda326e2370ec357b0ba17178aaf83ff
nandobfer <nandobfer@yahoo.com.br>
2023-05-29 18:51:01 -0300

    db: datetime

---
+++ prisma/migrations/20230529212556_orders_date/migration.sql
@@ -0,0 +1,26 @@
+-- DropIndex
+DROP INDEX `addresses_user_fkey` ON `addresses`;
+
+-- DropIndex
+DROP INDEX `cards_user_fkey` ON `cards`;
+
+-- DropIndex
+DROP INDEX `orders_user_id_fkey` ON `orders`;
+
+-- AlterTable
+ALTER TABLE `orders` MODIFY `date` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3);
+
+-- AddForeignKey
+ALTER TABLE `cards` ADD CONSTRAINT `cards_user_fkey` FOREIGN KEY (`user`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `addresses` ADD CONSTRAINT `addresses_user_fkey` FOREIGN KEY (`user`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `orders` ADD CONSTRAINT `orders_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_ordersToproducts` ADD CONSTRAINT `_ordersToproducts_A_fkey` FOREIGN KEY (`A`) REFERENCES `orders`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_ordersToproducts` ADD CONSTRAINT `_ordersToproducts_B_fkey` FOREIGN KEY (`B`) REFERENCES `products`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
