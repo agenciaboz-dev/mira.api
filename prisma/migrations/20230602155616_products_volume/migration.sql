51b95bb852d67a872498d2aefce44de986040792
nandobfer <nandobfer@yahoo.com.br>
2023-06-02 12:56:33 -0300

    db: added volume to products

---
+++ prisma/migrations/20230602155616_products_volume/migration.sql
@@ -0,0 +1,35 @@
+-- DropIndex
+DROP INDEX `addresses_user_fkey` ON `addresses`;
+
+-- DropIndex
+DROP INDEX `cards_user_fkey` ON `cards`;
+
+-- DropIndex
+DROP INDEX `orders_address_id_fkey` ON `orders`;
+
+-- DropIndex
+DROP INDEX `orders_user_id_fkey` ON `orders`;
+
+-- AlterTable
+ALTER TABLE `products` ADD COLUMN `height` DOUBLE NOT NULL DEFAULT 5,
+    ADD COLUMN `length` DOUBLE NOT NULL DEFAULT 5,
+    ADD COLUMN `weight` DOUBLE NOT NULL DEFAULT 5,
+    ADD COLUMN `width` DOUBLE NOT NULL DEFAULT 5;
+
+-- AddForeignKey
+ALTER TABLE `cards` ADD CONSTRAINT `cards_user_fkey` FOREIGN KEY (`user`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `addresses` ADD CONSTRAINT `addresses_user_fkey` FOREIGN KEY (`user`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `orders` ADD CONSTRAINT `orders_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `orders` ADD CONSTRAINT `orders_address_id_fkey` FOREIGN KEY (`address_id`) REFERENCES `addresses`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_ordersToproducts` ADD CONSTRAINT `_ordersToproducts_A_fkey` FOREIGN KEY (`A`) REFERENCES `orders`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_ordersToproducts` ADD CONSTRAINT `_ordersToproducts_B_fkey` FOREIGN KEY (`B`) REFERENCES `products`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
