b8367330e32eeaf3db4fc41b65a0d3b76ff7bc2f
nandobfer <nandobfer@yahoo.com.br>
2023-06-05 15:49:35 -0300

    test: pagseguro production

--- src/pagseguro.ts
+++ src/pagseguro.ts
@@ -2,11 +2,13 @@ import axios from "axios"
 import { Order } from "./definitions/pagseguro"
 
 const api = axios.create({
-    baseURL: "https://sandbox.api.pagseguro.com",
+    baseURL: "https://api.pagseguro.com",
+    // baseURL: "https://sandbox.api.pagseguro.com",
     timeout: 1000 * 10,
 })
 
-const token = "070B295159854F84A2358971995B2C6E"
+// const token = "070B295159854F84A2358971995B2C6E"
+const token = "a85eda2d-b4d5-498a-8321-7122124409cde86412e44669a7601efe4175e898a271a597-c60a-4d8b-88ee-969945e3682c"
 
 const headers = { Authorization: token }
 
