64a765057808487877458e7356b84fc661d1e310
nandobfer <nandobfer@yahoo.com.br>
2023-06-15 18:00:38 -0300

    feat: category handles image uploading

--- src/categories.ts
+++ src/categories.ts
@@ -1,5 +1,8 @@
 import express, { Express, Request, Response } from "express"
 import { PrismaClient } from "@prisma/client"
+import fileUpload from "express-fileupload"
+import { existsSync, mkdirSync } from "fs"
+import { join } from "path"
 const router = express.Router()
 const prisma = new PrismaClient()
 
@@ -9,14 +12,55 @@ router.get("/", async (request: Request, response: Response) => {
 })
 
 router.post("/add", async (request: Request, response: Response) => {
-    const data = request.body
+    const data = JSON.parse(request.body.data)
+    const imageFile = request.files?.file! as fileUpload.UploadedFile
 
-    const category = await prisma.categories.create({ data: { name: data.name }, include: { products: true } })
-    response.json(category)
+    const category = await prisma.categories.create({
+        data: { name: data.name, image: data.image },
+        include: { products: true },
+    })
+
+    const uploadDir = `images/categories/${category.id}`
+    if (!existsSync(uploadDir)) {
+        mkdirSync(uploadDir, { recursive: true })
+    }
+    const filepath = join(uploadDir, imageFile.name)
+
+    imageFile.mv(filepath)
+
+    const updatedCategory = await prisma.categories.update({
+        data: { image: `https://app.agenciaboz.com.br:4102/${filepath}` },
+        where: { id: category.id },
+        include: { products: true },
+    })
+
+    response.json(updatedCategory)
 })
 
 router.post("/update", async (request: Request, response: Response) => {
-    const data = request.body
+    const data = JSON.parse(request.body.data)
+    const imageFile = request.files?.file! as fileUpload.UploadedFile
+
+    if (imageFile) {
+        const uploadDir = `images/categories/${data.id}`
+        if (!existsSync(uploadDir)) {
+            mkdirSync(uploadDir, { recursive: true })
+        }
+
+        const filepath = join(uploadDir, imageFile.name)
+
+        imageFile.mv(filepath, (err) => {
+            console.log(err)
+        })
+
+        const imageCategory = await prisma.categories.update({
+            data: {
+                image: `https://app.agenciaboz.com.br:4102/${filepath}`,
+            },
+            where: { id: data.id },
+            include: { products: true },
+        })
+    }
 
     const category = await prisma.categories.update({
         where: { id: data.id },
