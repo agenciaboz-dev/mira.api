9176c0a0cbda0ec5ebde28bf8f372b2710cdaac0
Vitoria F <freitasvitoria712@gmail.com>
2023-07-19 11:50:14 -0300

    [add] rotas de password-recovery e password-reset

--- src/user.ts
+++ src/user.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express"
 import { PrismaClient } from "@prisma/client"
 import { clients } from "./websocket/socket"
+import nodemailer from 'nodemailer';
 
 const router = express.Router()
 const prisma = new PrismaClient()
@@ -117,5 +118,103 @@ router.post("/card", async (request: Request, response: Response) => {
 router.get("/ws", async (request: Request, response: Response) => {
     response.json(clients)
 })
+
+router.post("/password-recovery", async (request: Request, response: Response) => {
+    const { email } = request.body;
+      
+    const user = await prisma.users.findUnique({
+      where: { email },
+    });
+  
+    if (!user) {
+      return response.status(400).json({ error: "Usuário não encontrado." });
+    }
+  
+    const token = Math.random().toString(36).slice(-10);
+    const recoveryLink = `localhost:3000/password-reset?token=${token}`;
+    // https://app.mirasuprimentos.com.br/password-reset?token=${token}`;
+  
+    const transporterGmail = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'seu-email@gmail.com',
+        pass: 'sua-senha-do-email',
+      },
+    });
+
+    const transporterHotmail = nodemailer.createTransport({
+      service: 'hotmail',
+      auth: {
+        user: 'seu-email@hotmail.com',
+        pass: 'sua-senha-do-email',
+      },
+    });
+  
+    // Gmail
+    const mailOptionsGmail = {
+      from: 'seu-email@gmail.com',
+      to: user.email,
+      subject: 'Recuperação de senha',
+      html: `
+        <p>Olá ${user.name},</p>
+        <p>Clique <a href="${recoveryLink}">aqui</a> para redefinir sua senha.</p>
+      `,
+    };
+  
+    // Hotmail
+    const mailOptionsHotmail = {
+      from: 'seu-email@hotmail.com', 
+      to: user.email, 
+      subject: 'Recuperação de senha', 
+      html: `
+        <p>Olá ${user.name},</p>
+        <p>Clique <a href="${recoveryLink}">aqui</a> para redefinir sua senha.</p>
+      `,
+    };
+  
+    transporterGmail.sendMail(mailOptionsGmail, (error) => {
+      if (error) {
+        console.error(error);
+        response.status(500).json({ error: "Ocorreu um erro ao enviar o e-mail de recuperação de senha." });
+      } else {
+        response.json({ success: true });
+      }
+    });
+
+    transporterHotmail.sendMail(mailOptionsHotmail, (error) => {
+      if (error) {
+        console.error(error);
+        response.status(500).json({ error: "Ocorreu um erro ao enviar o e-mail de recuperação de senha." });
+      } else {
+        response.json({ success: true });
+      }
+    });
+});
+  
+
+router.post("/password-reset", async (request: Request, response: Response) => {
+    const { token, password } = request.body;
+  
+    const user = await prisma.users.findFirst({
+        where: {
+          token: { equals: token },
+        },
+    });
+    
+    if (!user) {
+      return response.status(400).json({ error: "Token inválido." });
+    }
+  
+    const updatedUser = await prisma.users.update({
+      where: { id: user.id },
+      data: {
+        password,
+        token,
+      },
+    });
+  
+    response.json({ message: "Senha redefinida com sucesso.", updatedUser });
+});
+  
 
 export default router
