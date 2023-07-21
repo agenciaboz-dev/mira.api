6a29fd02cb4a0f206068a904962da6c0c1d53d5e
Vitoria F <freitasvitoria712@gmail.com>
2023-07-21 08:57:31 -0300

    [add] teste rota

--- src/user.ts
+++ src/user.ts
@@ -184,6 +184,7 @@ router.post("/password-reset", async (request: Request, response: Response) => {
     where: { id: user.id },
     data: {
       password,
+      token: undefined,
     },
   });
 
