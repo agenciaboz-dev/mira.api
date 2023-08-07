928f5eaf0d4446bdb1a9afc6f4ad80cdb05c2b4b
ispadilha <israel.padilha@gmail.com>
2023-08-07 15:00:01 -0300

    Created draft for e-mail templates

--- src/templates/mail/password.ts
+++ src/templates/mail/password.ts
@@ -3,7 +3,7 @@ import { users } from "@prisma/client"
 export const passwordTemplate = (user: users, hash: string) => `
     <div>
         <p>${user.name},</p>
-        <p>Clique nesse link e insira sua nova senha</p>
-        <a href="https://app.mirasuprimentos.com.br/password/reset/${hash}">Resetar senha</a>
+        <p>Clique nesse link e insira sua nova senha:</p>
+        <a href="https://app.mirasuprimentos.com.br/password/reset/${hash}">Redefinir senha</a>
     </div>
 `
