e0a477f99cb364244f9d64593cb637beef926468
ispadilha <israel.padilha@gmail.com>
2023-08-09 11:20:16 -0300

    Finished e-mail templates

--- src/templates/mail/new_order.ts
+++ src/templates/mail/new_order.ts
@@ -1,9 +1,30 @@
 import { orders, users } from "@prisma/client"
 
 export const newOrderTemplate = (order: orders&{user:users}) => `
-    <div>
-        <p>${order.user.name},</p>
-        <p>Clique nesse link e insira sua nova senha</p>
-        <a href="https://app.mirasuprimentos.com.br/password/reset/">Resetar senha</a>
-    </div>
+<table width="100%" cellpadding="0" cellspacing="0" role="presentation" style="margin: 0;padding: 0;">
+<tbody>
+    <tr>
+        <td>
+            <table width="100%" cellpadding="0" cellspacing="0" role="presentation"
+                style="background: linear-gradient(to bottom, #b7b7b7, white);">
+                <tbody>
+                    <tr>
+                        <td style="padding: 6vw;">
+                            <p
+                                style="color: #53337d;font-size: 3.5em;font-family: sans-serif;font-weight: bold;margin: 0;">
+                                ${order.user.name}
+                            </p>
+                            <p style="font-size: 2.5em;font-family: sans-serif;">
+                                Seu novo pedido foi recebido.
+                                <br>
+                                Obrigado!
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </td>
+    </tr>
+</tbody>
+</table>
 `
--- src/templates/mail/password.ts
+++ src/templates/mail/password.ts
@@ -1,9 +1,32 @@
 import { users } from "@prisma/client"
 
 export const passwordTemplate = (user: users, hash: string) => `
-    <div>
-        <p>${user.name},</p>
-        <p>Clique nesse link e insira sua nova senha:</p>
-        <a href="https://app.mirasuprimentos.com.br/password/reset/${hash}">Redefinir senha</a>
-    </div>
+<table width="100%" cellpadding="0" cellspacing="0" role="presentation" style="margin: 0;padding: 0;">
+<tbody>
+    <tr>
+        <td>
+            <table width="100%" cellpadding="0" cellspacing="0" role="presentation"
+                style="background: linear-gradient(to bottom, #b7b7b7, white);">
+                <tbody>
+                    <tr>
+                        <td style="padding: 6vw;">
+                            <p
+                                style="color: #53337d;font-size: 3.5em;font-family: sans-serif;font-weight: bold;margin: 0;">
+                                ${user.name}
+                            </p>
+                            <p style="font-size: 2.5em;font-family: sans-serif;">
+                                Clique nesse link e insira sua nova senha:
+                            </p>
+                            <a href="https://app.mirasuprimentos.com.br/password/reset/${hash}"
+                                style="font-size: 2.5em;font-family: sans-serif;">
+                                Redefinir senha
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </td>
+    </tr>
+</tbody>
+</table>
 `
